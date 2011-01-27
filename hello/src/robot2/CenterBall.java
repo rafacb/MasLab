5f5c5623d1e7e9ca3cee230a7de76056fd786ce8
Rafael Crespo <cresporafa@gmail.com>
2011-01-27 06:01:05 -0500

    Mas tests para center 4.

--- hello/src/robot2/CenterBall.java
+++ hello/src/robot2/CenterBall.java
@@ -15,11 +15,13 @@ public class CenterBall {
 				int[] pos = robot.ball_pos;
 				if (pos[0] > width + 50) {
 					robot.move(new double[] {Double.valueOf(args[0]),-Double.valueOf(args[1])});
+					robot.move(new double[] {1000,0});
+					Thread.sleep(100);
 				} else if (pos[0] < width - 50) {
 					robot.move(new double[] {-Double.valueOf(args[0]),Double.valueOf(args[1])});
-				}
-				robot.move(new double[] {1000,0});
-				Thread.sleep(100);
+					robot.move(new double[] {1000,0});
+					Thread.sleep(100);
+				} 			
 			}
 		}
 		
