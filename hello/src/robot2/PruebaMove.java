85f31ae46d6bf1eda651093912d2d3f664034483
N'Sync <junkmail.cuenta@gmail.com>
2011-01-26 23:25:05 -0500

    codigo de probar movimiento toma argumentos lo que lo hace util y facil de usar.

--- hello/src/robot2/PruebaMove.java
+++ hello/src/robot2/PruebaMove.java
@@ -7,7 +7,7 @@ public class PruebaMove {
 	public static void main(String[] args) throws IOException,
 	InterruptedException {
 		Robot r = new Robot("red");
-		double[] speed = {.5,.5};
+		double[] speed = {Double.valueOf(args[0]),Double.valueOf(args[1])};
 		for (int i = 0; i<1000; i++){
 			r.move(speed);
 		}
