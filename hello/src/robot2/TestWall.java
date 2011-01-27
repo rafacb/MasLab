1d701215d0d1fe1f2f97026944ef295296b96fa0
Rafael Crespo <cresporafa@gmail.com>
2011-01-27 16:58:42 -0500

    Mas tests para center 12.

--- hello/src/robot2/TestWall.java
+++ hello/src/robot2/TestWall.java
@@ -17,6 +17,21 @@ public class TestWall {
 				}for (int i=0; i<1750;i++){
 				    robot.move(new double[] {.5,-.5});
 				}
+				Thread.sleep(80);
+				while (robot.rearBump.getVoltage() > .2){
+					robot.move(new double[] {.5, .5});
+				}
+				//Servo shit!
+				for (int i = 0; i < 4000; i++){
+					robot.moveServo(Math.PI*.75);
+					System.out.println("Mas Arriba!");
+				}for (int i = 0; i < 1000; i++){
+					robot.moveServo(Math.PI*.4);
+					System.out.println("Arriba!");
+				}for (int i = 0; i < 1000; i++){
+					robot.moveServo(1000);
+					System.out.println("Muere!!");
+				}
 			}
 	    }
 		
