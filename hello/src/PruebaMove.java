bb6e22a6f645b603848818b31adaa127eeed5ec1
Rafael Crespo <cresporafa@gmail.com>
2011-01-28 00:02:50 -0500

    Combo 24.

--- hello/src/PruebaMove.java
+++ hello/src/PruebaMove.java
@@ -5,7 +5,7 @@ public class PruebaMove {
 	public static void main(String[] args) throws IOException,
 	InterruptedException {
 		Robot r = new Robot("red");
-		double[] speed = {Double.valueOf(args[0]),Double.valueOf(args[1])};
+		double[] speed = {Double.valueOf(args[0]),Double.valueOf(args[1]), 0};
 		for (int i = 0; i<1000; i++){
 			r.move(speed);
 		}
